d6858798507d8a9c2c5416be8f3cee65dcadd29e
ryota-murakami <dojce1048@gmail.com>
2018-10-17 21:04:08 +0900

    add cli help & version option

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 #!/usr/bin/env node
 
+const cli = require(`yargs`)
 var chalk = require('chalk')
 var exec = require('child_process').exec
 var execSync = require('child_process').execSync
 
+cli
+  .usage(`Usage: $0`)
+  .alias(`h`, `help`)
+  .alias(`v`, `version`)
+  .help().argv
+
 // get 'flow-bin' version
-exec('yarn why flow-bin', { stdio: [0, 1, 2] }, (error, stdout, stderr) => {
+exec('yarn why flow-bin', { stdio: [0, 1, 2] }, (error, stdout) => {
   if (error) {
     console.error(chalk.bgRed.bold(`exec error: ${error}`))
     process.exit(1)
   }
-  if (stderr) console.log(chalk.bgRed.bold(`stderr: ${stderr}`))
 
   // Found "flow-bin@0.xx.0" from "yarn why flow-bin" output string
   if (stdout) {
     const cap = stdout.match(/flow-bin@[0-9]\.[0-9]{2,}\.[0-9]/)
     if (cap === null) {
       console.error(
-        chalk.bgRed.bold(
-          'flow-bin not found in package.json. please make sure "yarn add -D flow-bin".'
+        chalk.red(
+          '\nflow-bin not found in package.json. please try "yarn add -D flow-bin".\n'
         )
       )
       process.exit(1)
