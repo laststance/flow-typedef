2e1928a1e7dd95d102715745b17fe70c1ef2b841
ryota-murakami <dojce1048@gmail.com>
2018-11-03 20:26:42 +0900

    feat: implove execution speed from the second time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
 const cli = require(`yargs`)
-var chalk = require('chalk')
-var exec = require('child_process').exec
-var execSync = require('child_process').execSync
+const chalk = require('chalk')
+const exec = require('child_process').exec
+const execSync = require('child_process').execSync
 
 cli
   .usage(`Usage: $0`)
@@ -11,47 +12,80 @@ cli
   .alias(`v`, `version`)
   .help().argv
 
-// get 'flow-bin' version
+// Confirm whether installed 'flow-bin' and get version info
 exec('yarn why flow-bin', { stdio: [0, 1, 2] }, (error, stdout) => {
   if (error) {
     console.error(chalk.bgRed.bold(`exec error: ${error}`))
     process.exit(1)
   }
 
-  // Extract "flow-bin@0.xx.0" from "yarn why flow-bin" output string
-  if (stdout) {
-    const cap = stdout.match(/flow-bin@[0-9]\.[0-9]{2,}\.[0-9]/)
-    if (cap === null) {
-      console.error(
-        chalk.red(
-          '\nflow-bin not found in package.json. please try "yarn add -D flow-bin".\n'
-        )
-      )
-      process.exit(1)
-    }
-    // Extract "0.xx.0" from "flow-bin@0.xx.0"
-    const semver = cap[0].split('@')[1]
+  /**
+   * Prepare Flow repository
+   */
 
-    // git clone flow@0.xx.0
-    execSync(`git clone --branch v${semver} git@github.com:facebook/flow.git`, {
+  const isFlowExists = fs.existsSync(__dirname + '/flow')
+  if (isFlowExists) {
+    execSync(`cd ${__dirname} && git pull origin master`, {
       stdio: [0, 1, 2]
     })
-
-    // Create flow-typed/build-in directory
-    execSync('mkdir -p flow-typed/built-in', { stdio: [0, 1, 2] })
-
-    // Copy raw typedef files flow repo to my repo
-    execSync('cp flow/lib/* flow-typed/built-in', { stdio: [0, 1, 2] })
-    execSync('rm -rf flow', { stdio: [0, 1, 2] })
-
-    // Copy UtilityTypes
-    execSync(`cp ${__dirname}/UtilityTypes.js flow-typed/UtilityTypes.js`, {
+  } else {
+    execSync(`cd ${__dirname} && git clone git@github.com:facebook/flow.git`, {
       stdio: [0, 1, 2]
     })
-
-    console.log(
-      chalk.green.bold('\n\nInstalled Flow built-in type definition!\n')
-    )
-    process.exit(0)
   }
+
+  /**
+   * Specify installed Flow version
+   */
+
+  // Extract "flow-bin@0.xx.0" from "yarn why flow-bin" output string
+  const cap = stdout.match(/flow-bin@[0-9]\.[0-9]{2,}\.[0-9]/)
+  if (cap === null) {
+    console.error(
+      chalk.red(
+        '\nflow-bin not found in package.json. please try "yarn add -D flow-bin".\n'
+      )
+    )
+    process.exit(1)
+  }
+
+  // Extract "0.xx.0" from "flow-bin@0.xx.0"
+  const semver = cap[0].split('@')[1]
+
+  // Make same semver repository
+  execSync(`cd ${__dirname}/flow && git checkout v${semver}`, {
+    stdio: [0, 1, 2]
+  })
+
+  /**
+   * Put Flow built-in type definition files
+   */
+
+  // Create flow-typed/build-in directory
+  execSync('mkdir -p flow-typed/built-in', { stdio: [0, 1, 2] })
+
+  // Copy raw typedef files flow repo to my repo
+  execSync(`cp ${__dirname}/flow/lib/* flow-typed/built-in`, {
+    stdio: [0, 1, 2]
+  })
+
+  // Copy UtilityTypes
+  execSync(`cp ${__dirname}/UtilityTypes.js flow-typed/UtilityTypes.js`, {
+    stdio: [0, 1, 2]
+  })
+
+  /**
+   * Finish
+   */
+
+  // Return from detached HEAD
+  execSync(`cd ${__dirname}/flow && git checkout master`, {
+    stdio: [0, 1, 2]
+  })
+
+  console.log(
+    chalk.green.bold('\n\nInstalled Flow built-in type definition!\n')
+  )
+
+  process.exit(0)
 })
